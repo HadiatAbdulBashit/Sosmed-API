332c01b72edd1585e3a39a26650db4473375bd97
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2024-08-14 20:29:14 +0700

    initial commit

---
+++ src/test/java/com/hadiat/sosmed/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadiat.sosmed.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
